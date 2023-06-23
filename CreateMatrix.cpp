29fa3f57d04e6e616481ed494b677cddbafe3d77
Piotr98765 <61555492+Piotr98765@users.noreply.github.com>
2023-06-23 19:35:42 +0200

    Update
    Update

--- CreateMatrix.cpp
+++ CreateMatrix.cpp
@@ -3,36 +3,60 @@
 #include <ctime>
 using namespace std;
 
-int main() {
-	srand(time(0));
+
+class Matrix {
+private:
+	int rows;
+	int columns;
+	int** data;
+public:
+	Matrix(int numRows, int numColumns) : rows(numRows), columns(numColumns){
+		data = new int*[rows];
+		for (int i = 0; i < rows; i++) {
+			data[i] = new int[columns];
+		}
+	}
+
+	~Matrix() {
+		for (int i = 0; i < rows; i++){
+			delete[] data[i];
+		}
+		delete[] data;
+	}
+
+	void fillWithNums() {
+	    for (int i = 0; i < rows; i++) {
+	        for (int j = 0; j < columns; j++) {
+	            data[i][j] = rand() % 100;
+	        }
+	    }
+	}
+
+	void display() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				cout << data[i][j] << " ";
+			}
+			cout << "\n";
+		}
+	}
+};
+
+
+int main() { 
+	int numMatrices = 3;
 	int rows = 3;
 	int columns = 3;
+	srand(time(0));
+	for (int k=0; k < numMatrices; k++) {
+		Matrix matrix(rows, columns);
+		matrix.fillWithNums();
 
-	int** matrix = new int*[rows];
 
-	for (int i = 0; i < rows; i++){
-		matrix[i] = new int[columns];
-	}
-
-	for (int i=0; i < rows; i++){
-		for (int j=0; j < columns; j++){
-			matrix[i][j]= rand() % 100; 
-		}
-	}
-
-	for (int i=0; i < rows; i++){
-		for (int j=0; j < columns; j++){
-			cout << matrix[i][j] << " ";
-		}
+		cout << "Matrix " << k+1 << "\n";
+		matrix.display();
 		cout << "\n";
 	}
 
-
-	for (int i = 0; i < rows; i++){
-		delete[] matrix[i];
-	}
-	delete[] matrix;
-
 	return 0;
 }
-
