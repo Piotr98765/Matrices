b6d4de8f16a36759d3542f27d7303178b8e58f0c
Piotr98765 <61555492+Piotr98765@users.noreply.github.com>
2023-06-24 16:15:02 +0200

    Update
    Added: addition function, a few more little updates

--- CreateMatrix.cpp
+++ CreateMatrix.cpp
@@ -2,72 +2,98 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <random>
 using namespace std;
-
 
 class Matrix {
 private:
-	int rows;
-	int columns;
-	int** data;
+    int rows;
+    int columns;
+    vector<int> data;
+
 public:
-	Matrix(int numRows, int numColumns) : rows(numRows), columns(numColumns){
-		data = new int*[rows];
-		for (int i = 0; i < rows; i++) {
-			data[i] = new int[columns];
-		}
-	}
+    Matrix(int numRows, int numColumns) : rows(numRows), columns(numColumns), data(numRows * numColumns) {}
 
-	void fillWithNums() {
-	    for (int i = 0; i < rows; i++) {
-	        for (int j = 0; j < columns; j++) {
-	            data[i][j] = rand() % 100;
-	        }
-	    }
-	}
+    void fillWithNums() {
+        random_device rd;
+        mt19937 generator(rd());
+        uniform_int_distribution<int> distribution(0, 99);
 
-	void display() {
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				cout << data[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                data[i * columns + j] = distribution(generator);
+            }
+        }
+    }
+
+    friend void displayMatrix(const Matrix& matrix);
+    friend void matrixAddition(vector<Matrix>& matrices, const Matrix& matrix1, const Matrix& matrix2);
 };
 
+void displayMatrix(const Matrix& matrix) {
+    int rows = matrix.rows;
+    int columns = matrix.columns;
 
-int main() { 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cout << matrix.data[i * columns + j] << " ";
+        }
+        cout << "\n";
+    }
+}
 
-	int numMatrices;
-	cout << "How many matrices? ";
-	cin >> numMatrices;
-	cout << "\n";
+void matrixAddition(vector<Matrix>& matrices, const Matrix& matrix1, const Matrix& matrix2) {
+    if (matrix1.rows != matrix2.rows || matrix1.columns != matrix2.columns) {
+        cout << "Matrices have incompatible dimensions for addition.";
+        return;
+    }
 
-	int rows;
-	cout << "How many rows in each matrix? ";
-	cin >> rows;
-	cout << "\n";
+    Matrix result(matrix1.rows, matrix1.columns);
 
-	int columns;
-	cout << "How many columns in each matrix? ";
-	cin >> columns;
-	cout << "\n";
+    for (int i = 0; i < matrix1.rows; i++) {
+        for (int j = 0; j < matrix1.columns; j++) {
+            result.data[i * matrix1.columns + j] = matrix1.data[i * matrix1.columns + j] +
+                                                   matrix2.data[i * matrix1.columns + j];
+        }
+    }
 
-	srand(time(0));
+    cout << "Result of addition: " << "\n";
+    matrices.push_back(result);
+    displayMatrix(result);
+}
 
-	vector<Matrix> matrices;
+int main() {
+    int numMatrices;
+    cout << "How many matrices? ";
+    cin >> numMatrices;
+    cout << "\n";
 
-	for (int k=0; k < numMatrices; k++) {
-		Matrix matrix(rows, columns);
-		matrix.fillWithNums();
-		matrices.push_back(matrix);
-	}
+    int rows;
+    cout << "How many rows in each matrix? ";
+    cin >> rows;
+    cout << "\n";
 
-	for (int k = 0; k < numMatrices; k++) {
-		cout << "This is " << k + 1 << " matrix." << "\n";
-		matrices[k].display();
-	}
+    int columns;
+    cout << "How many columns in each matrix? ";
+    cin >> columns;
+    cout << "\n";
 
-	return 0;
+    srand(time(0));
+
+    vector<Matrix> matrices;
+
+    for (int k = 0; k < numMatrices; k++) {
+        Matrix matrix(rows, columns);
+        matrix.fillWithNums();
+        matrices.push_back(matrix);
+    }
+
+    for (int k = 0; k < numMatrices; k++) {
+        cout << "This is " << k + 1 << " matrix." << "\n";
+        displayMatrix(matrices[k]);
+    }
+
+    matrixAddition(matrices, matrices[0], matrices[1]);
+
+    return 0;
 }
